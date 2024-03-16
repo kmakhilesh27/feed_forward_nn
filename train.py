382e3d674a4af8afaedda09f6596c412df8e3ee0
akhileshkumar07 <akhilesh.kumar070197@gmail.com>
2024-03-16 16:47:04 +0530

    Main file to simulate a neural network run

--- train.py
+++ train.py
@@ -1,107 +1,55 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
+from dataloader import load_and_process_dataset
+from argparser import create_parser
 import neural_network as nn
-from arg_parser import create_parser
 
-from keras.utils import to_categorical
-from keras.datasets import fashion_mnist, mnist
+import matplotlib.pyplot as plt
+import numpy as np
 
-import wandb
-from types import SimpleNamespace
+parser = create_parser()
+args = vars(parser.parse_args())
 
-###############################################
+x_train, y_train , x_test, y_test = load_and_process_dataset(choice = args['dataset'])
 
-def visualize_data(x_train, y_train):    
-    n_class = np.unique(y_train)
+model = nn.FeedForwardNN(input_size=784, num_hidden_layers=args['num_layers'],
+                             hidden_size=args['hidden_size'], output_size=10,
+                             weight_initializer=args['weight_init'])
+model.train(x_train, y_train,
+            epochs = args['epochs'],
+                    batch_size = args['batch_size'],
+                    lossfn = args['loss'],
+                    optimizer = args['optimizer'],
+                    lr = args['learning_rate'],
+                    momentum = args['momentum'],
+                    beta = args['beta'],
+                    beta1 = args['beta1'],
+                    beta2 = args['beta2'],
+                    eps = args['epsilon'],
+                    weight_decay = args['weight_decay'],
+                    activationfn = args['activation']
+                    )
+model.predict(x_test, y_test,args['activation'])
 
-    _, axs = plt.subplots(10, 10, figsize=(10, 10))
-    
-    for i, d in enumerate(n_class):
-        for j in range(len(n_class)):
-            axs[j, i].imshow(x_train[y_train == d][j])
-            axs[j, i].axis('off')         
+parameters = model.get_parameters()
+train_loss = model.get_training_loss()
+val_loss = model.get_validation_loss()
+train_acc = model.get_training_accuracy()
+val_acc = model.get_validation_accuracy()
 
-    plt.show()
-    return
+# Plotting the training and validation loss
+plt.figure()
+num_epochs = len(train_loss)
+plt.plot(np.arange(num_epochs), train_loss, color='blue', label='Training Loss')
+plt.plot(np.arange(num_epochs), val_loss, color='red', label='Validation Loss')
+plt.xlabel('Number of Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
 
-def load_and_visualize_dataset(choice = "fashion_mnist"):
-
-    if choice == "fashion_mnist":
-        (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
-    elif choice == "mnist":
-        (x_train, y_train), (x_test, y_test) = mnist.load_data()
-    else:
-        raise Exception("Invalid dataset.")    
-    visualize_data(x_train, y_train)
-    
-    return x_train, y_train , x_test, y_test
-
-def process_dataset(x_train, y_train, x_test, y_test):
-
-    # reshape the images into n-dimensional vector
-    x_train = x_train.reshape(x_train.shape[0], -1)    
-    x_test = x_test.reshape(x_test.shape[0], -1)
-
-    # normalize x_train and x_test pixel values
-    x_train = x_train / 255.0
-    x_test = x_test / 255.0
-
-    # one-hot encode y_train and y_test values
-    y_train = to_categorical(y_train)
-    y_test = to_categorical(y_test)
-
-    return x_train, y_train , x_test, y_test
-
-if __name__=="__main__":
-   
-    parser = create_parser()
-    args = vars(parser.parse_args())
-
-    wandb.login(key='68d634810410e6fa1baf445fbc2ab0f586e0272e')
-
-    sweep_config = {
-    'method': 'grid',
-    'name' : 'sweep cross entropy',
-    'metric': {
-      'name': 'val_accuracy',
-      'goal': 'maximize'
-    },
-    'parameters': {'epochs': {'values': [5,10]},
-                   'num_hidden_layers': {'values': [3,4,5]},
-                   'hidden_size':{'values':[32,64,128]},
-                   'weight_decay': {'values': [0, 0.0005, 0.5]},
-                   'learning_rate': {'values': [1e-3, 1e-4]},
-                   'optimizer': {'values': ['sgd', 'momentum', 'nesterov', 'rmsprop', 'adam', 'nadam']},
-                   'batch_size': {'values': [16,32,64]},
-                   'weight_init': {'values': ['random','xavier']},
-                   'activation': {'values': ['sigmoid','ReLU','tanh']},
-                   'loss': {'values': ['cross_entropy']}
-                   }
-    }
-
-    sweep_id = wandb.sweep(sweep=sweep_config, project='CS6910_Assignment_1')
-
-
-    #args = {'wandb_project': 'myprojectname', 'wandb_entity': 'myname', 'dataset': 'fashion_mnist',
-    #        'epochs': 1, 'batch_size': 4, 'loss': 'cross_entropy', 'optimizer': 'sgd', 'learning_rate': 0.1,
-    #       'momentum': 0.5, 'beta': 0.5, 'beta1': 0.5, 'beta2': 0.5, 'epsilon': 1e-06, 'weight_decay': 0.0,
-    #       'weight_init': 'random', 'num_layers': 1, 'hidden_size': 4, 'activation': 'sigmoid'}
-
-    
-    x_train, y_train , x_test, y_test = load_and_visualize_dataset(choice = args['dataset'])    
-    x_train, y_train , x_test, y_test = process_dataset(x_train, y_train , x_test, y_test)
-
-    with wandb.init() as run:
-
-        run_name="-ac_"+wandb.config.activation+"-hs"+str(wandb.config.hidden_size)
-        wandb.run.name=run_name
-        
-        model = nn.FeedForwardNN(input_size=x_train.shape[-1], num_hidden_layers=args['num_layers'],
-                              hidden_size=args['hidden_size'], output_size=10, weight_initializer=args['weight_init'])
-        model.train(args, x_train, y_train)
-        model.predict(x_test, y_test, activationfn=args['activation'])
-
-    wandb.agent(sweep_id, count=10) # calls main function for count number of times.
-    wandb.finish()
-    
+plt.figure()
+num_epochs = len(train_acc)
+plt.plot(np.arange(num_epochs), train_acc, color='blue', label='Training Accuracy')
+plt.plot(np.arange(num_epochs), val_acc, color='red', label='Validation Accuracy')
+plt.xlabel('Number of Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
